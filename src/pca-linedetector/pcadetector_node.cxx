ed1d8652f16fc14e3bdd41715dfc9874c69bdc56
Harsh Sinha <harshsin@iitk.ac.in>
2017-12-01 15:23:25 +0530

    adding the pca line detector

--- src/pca-linedetector/pcadetector_node.cxx
+++ src/pca-linedetector/pcadetector_node.cxx
@@ -11,6 +11,10 @@
 cv::Mat frame;
 cv::VideoCapture cap;
 
+/* Colors */
+auto yellow_low  = cv::Scalar(20, 100, 100);
+auto yellow_high = cv::Scalar(30, 255, 255);
+
 /* Call back function for image */
 void imcallback (const sensor_msgs::ImageConstPtr& msg)
 {
@@ -18,6 +22,24 @@ void imcallback (const sensor_msgs::ImageConstPtr& msg)
     frame = cv_bridge::toCvShare (msg) -> image;
     return;
 
+}
+
+/* PCA : returns lines along first and second principle components */
+cv::Vec4i PCA (std::vector<cv::Vec2i> &data_points)
+{
+
+    auto mean = cv::mean(data_points);
+    std::vector<cv::Vec2i> data;
+    for (std::vector<cv::Vec2i>::iterator it = data_points.begin(); it != data_points.end(); ++it) {
+
+        auto d = *it;
+        data.push_back(cv::Vec2i(d[0] - mean[0], d[1] - mean[1]));
+    }
+    
+    std::cout << mean[0] << mean[1] << std::endl;
+    // data_points = data_points - cv::Vec2i(mean[0], mean[1]);
+    // std::cout << mean << std::endl;
+    return (cv::Vec4i(1, 2, 3, 4));
 }
 
 int main (int argc, char** argv)
@@ -38,8 +60,12 @@ int main (int argc, char** argv)
     /* Choose camera */
 	int camera = argv [1][0] - 48;
 
-    /* Image size, Parameters for SLIC */
+    /* Parameters for Regular Grid */
+    int n_grid = 16;
     auto image_size = cv::Size(256, 256);
+    auto grid_size = image_size/n_grid;
+
+    /* Image size, Parameters for SLIC */
     int nr_superpixels = 100;
     int nc = 25;
 
@@ -65,14 +91,40 @@ int main (int argc, char** argv)
 
         if (!frame.empty()) {
 
+            /* Empty Images and Lines */
+            cv::Mat hsv, thresh, blurred, opening, closing, result, temp, final_image;
+            std::vector<cv::Vec4i> lines;
+            std::vector<cv::Vec2i> points;
+
             /* Load, Resize and Convert image */
-            cv::resize (frame, frame, image_size);
-            auto lab_image = frame.clone();
-            auto mean_image = frame.clone();
-            cv::cvtColor (frame, lab_image, CV_BGR2Lab);
+            cv::resize(frame, frame, image_size);
+            cv::cvtColor(frame, hsv, CV_BGR2HSV);
+            cv::inRange(hsv, yellow_low, yellow_high, thresh);
+			cv::GaussianBlur (thresh, blurred,  cv::Size(11, 11), 0, 0);
+			cv::morphologyEx (blurred, closing, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2),  cv::Point(-1, -1)));
+			cv::morphologyEx (closing, opening, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2),  cv::Point(-1, -1)));
+            cv::Canny (thresh, result, 50, 150, 3);
+
+            cv::HoughLinesP(result, lines, 5, CV_PI/180, 10, 10);
+
+            /* PCA on the point from Hough lines */
+            int i = 0;
+            for (std::vector<cv::Vec4i>::iterator it = lines.begin(); it != lines.end(); ++it) {
+
+                cv::Vec4i l = *it;
+                // points.push_back(cv::Vec2i(l[0], l[1]));
+                // points.push_back(cv::Vec2i(l[2], l[3]));
+                points.push_back(cv::Vec2i(i, i + 1));
+                ++i;
+            }
+
+            auto principle_lines = PCA (points);
 
 /*******************************************************************/
 /* SLIC not good, fails on mean calculation */
+            /*auto lab_image = frame.clone();
+            cv::cvtColor (frame, lab_image, CV_BGR2Lab);*/
+            
             /* Number of super-pixels and weight factors *
             int w = frame.rows, h = frame.cols;
             double step = std::sqrt((w * h) / nr_superpixels);
@@ -87,8 +139,8 @@ int main (int argc, char** argv)
 /*******************************************************************/
 
             cv::imshow("image", frame);
-            pixelLine.x = 1;
-            pub.publish(pixelLine);
+            cv::imshow("opening", opening);
+            cv::imshow("canny", result);
 
             if (cv::waitKey(1) == 113)
 			break;
