de0319d45bf4fa83b1a8d54349e7f6b574c7e59b
Harsh Sinha <harshsin@iitk.ac.in>
2017-12-01 19:03:48 +0530

    pca done, testing required

--- src/pca-linedetector/pcadetector_node.cxx
+++ src/pca-linedetector/pcadetector_node.cxx
@@ -25,21 +25,47 @@ void imcallback (const sensor_msgs::ImageConstPtr& msg)
 }
 
 /* PCA : returns lines along first and second principle components */
-cv::Vec4i PCA (std::vector<cv::Vec2i> &data_points)
+cv::Vec4f PCA (std::vector<cv::Vec2i> &data_points)
 {
 
+    if(data_points.size() == 0)
+        return (cv::Vec4i(-1, -1, -1, -1));
+
+    cv::Vec4f vectors;
     auto mean = cv::mean(data_points);
-    std::vector<cv::Vec2i> data;
+    cv::Mat data_transp(2, data_points.size(), CV_64F, cv::Scalar::all(0));
+    cv::Mat data(data_points.size(), 2, CV_64F, cv::Scalar::all(0));
+    
+    int i = 0;
     for (std::vector<cv::Vec2i>::iterator it = data_points.begin(); it != data_points.end(); ++it) {
 
         auto d = *it;
-        data.push_back(cv::Vec2i(d[0] - mean[0], d[1] - mean[1]));
+        data.at<double>(i, 0) = d[0] - mean[0];
+        data.at<double>(i, 1) = d[1] - mean[1];
+        data_transp.at<double>(0, i) = d[0] - mean[0];
+        data_transp.at<double>(1, i) = d[1] - mean[1];
+        ++i;
     }
+
+    // std::cout << data << std::endl;
+    // std::cout << "***" << std::endl;
+    // std::cout << data_transp << std::endl;
+    cv::Mat cov = data_transp * data;
+    // std::cout << "covariance : " << cov << std::endl;
+    cv::Mat eigenval, eigenvec;
+    cv::eigen(cov, eigenval, eigenvec);
+    // std::cout << "eigen vectors : " << eigenvec << std::endl;
+    // std::cout << "***********************************************" << std::endl;
     
-    std::cout << mean[0] << mean[1] << std::endl;
-    // data_points = data_points - cv::Vec2i(mean[0], mean[1]);
-    // std::cout << mean << std::endl;
-    return (cv::Vec4i(1, 2, 3, 4));
+    for(int i = 0; i < eigenvec.rows; ++i) {
+
+        const double * veci = eigenvec.ptr<double>(i);
+        vectors[i] = veci[1]/veci[0]?veci[0]:0.0001;
+        vectors[i+1] = mean[1] - (vectors[i] * mean[0]);
+    }
+
+    std::cout << vectors << std::endl;
+    return (vectors);
 }
 
 int main (int argc, char** argv)
@@ -105,21 +131,33 @@ int main (int argc, char** argv)
 			cv::morphologyEx (closing, opening, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(2, 2),  cv::Point(-1, -1)));
             cv::Canny (thresh, result, 50, 150, 3);
 
-            cv::HoughLinesP(result, lines, 5, CV_PI/180, 10, 10);
+            cv::HoughLinesP(result, lines, 5, CV_PI/180, 1, 1);
 
             /* PCA on the point from Hough lines */
-            int i = 0;
             for (std::vector<cv::Vec4i>::iterator it = lines.begin(); it != lines.end(); ++it) {
 
                 cv::Vec4i l = *it;
-                // points.push_back(cv::Vec2i(l[0], l[1]));
-                // points.push_back(cv::Vec2i(l[2], l[3]));
-                points.push_back(cv::Vec2i(i, i + 1));
-                ++i;
+                points.push_back(cv::Vec2i(l[0], l[1]));
+                points.push_back(cv::Vec2i(l[2], l[3]));
+                cv::circle(frame, cv::Point(l[0], l[1]), 5, cv::Scalar(255, 0, 0));
+                cv::circle(frame, cv::Point(l[2], l[3]), 5, cv::Scalar(255, 0, 0));
+
             }
 
             auto principle_lines = PCA (points);
 
+            auto m1_ = principle_lines[0];
+            auto c1_ = principle_lines[1];
+            auto m2_ = principle_lines[2];
+            auto c2_ = principle_lines[3];
+
+
+            /* Slopes of the two principle components can not be same */
+            if(m1_ != m2_) {
+
+                cv::line(frame, cv::Point(0, c1_), cv::Point(-c1_/m1_?m1_:0.01, 0), cv::Scalar(0, 0, 255), 2);
+                cv::line(frame, cv::Point(0, c2_), cv::Point(-c2_/m2_?m2_:0.01, 0), cv::Scalar(0, 0, 255), 2);
+            }
 /*******************************************************************/
 /* SLIC not good, fails on mean calculation */
             /*auto lab_image = frame.clone();
@@ -141,6 +179,8 @@ int main (int argc, char** argv)
             cv::imshow("image", frame);
             cv::imshow("opening", opening);
             cv::imshow("canny", result);
+
+            pub.publish(pixelLine);
 
             if (cv::waitKey(1) == 113)
 			break;
