3d0ed11e581ab95196985210f730437b06d579e0
jay <patrikarjay@gmail.com>
2017-12-17 17:00:16 +0530

    added line sub in controller

--- controller/src/controller.cpp
+++ controller/src/controller.cpp
@@ -3,12 +3,16 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "sensor_msgs/Range.h"
 #include <warehouse_quad/markerInfo.h>
+#include <warehouse_quad/line.h>
 float z;
 bool _detected=false;
-
+warehouse_quad::line line;
 using namespace std;
 
-
+void linecb(const warehouse_quad::line::ConstPtr& msg)
+{
+  line.mode = msg->mode;
+}
 void posecb(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
   z = msg->pose.position.z;
@@ -24,6 +28,7 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   ros::Subscriber sub = n.subscribe("/mavros/vision_pose/pose", 10, posecb);
   ros::Subscriber sub_marker = n.subscribe("/marker", 10, markercb);
+  ros::Subscriber sub_line = n.subscribe("/line",10, linecb);
   ros::Publisher setpoint_pub = n.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 10);
   ros::Publisher distance_pub = n.advertise<sensor_msgs::Range>("/mavros/distance_sensor/sonar_1_sub", 10);
   ros::Rate loop_rate(100);
@@ -55,7 +60,7 @@ int main(int argc, char **argv)
 
       case 2:
   	    setpoint.pose.position.z = 1.0f;
-        distance.radiation_type = 1;
+        distance.radiation_type = 0;
 	//	cout << ros::Time::now().toSec()<<endl;
 		cout << _detected << endl;
         if (0)
