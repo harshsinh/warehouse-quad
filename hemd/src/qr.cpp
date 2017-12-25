7552e47c3d9575ca791dc983ce0df001286d2a58
Harsh <harshsin@iitk.ac.in>
2017-12-25 18:57:47 +0530

    qr-code detection done

--- hemd/src/qr.cpp
+++ hemd/src/qr.cpp
@@ -1,19 +1,19 @@
 #include <ros/ros.h>
 #include <boost/thread.hpp>
 
-#include "kalman.h"
+#include "marker.h"
 
 int main(int argc, char **argv){
 
-	ros::init(argc, argv, "estimation");
+	ros::init(argc, argv, "detection");
 	ros::start();
 
     //call ekf for height and orientation estimation
-	HMDETECTION::EKF ekf;
-	boost::thread height(&HMDETECTION::EKF::subscriber, &ekf);
+//	HMDETECTION::EKF ekf;
+//	boost::thread height(&HMDETECTION::EKF::subscriber, &ekf);
 
-//HMDETECTION::MARKER marker;
-//	boost::thread detection(&HMDETECTION::MARKER::subscriber, &marker);
+	HMDETECTION::MARKER marker;
+	boost::thread detection(&HMDETECTION::MARKER::subscriber, &marker);
 	ros::Rate loopFreq(100);
 	while(ros::ok()){
 
